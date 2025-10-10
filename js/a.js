aadeec82bb0d1d8059320eacde7cb593553577cf
Gilmar Ferreira Terres Correa <gilmarftc@gmail.com>
2025-10-09 23:40:36 -0300

    ajustes precisos no conversor

--- js/a.js
+++ js/a.js
@@ -1,142 +1,222 @@
-// --- DADOS DA TABELA DE COEFICIENTES (P, R, S, T, U) ---
-const COEFICIENTES_TABELA = [
-    { min: 0.0000, R: -0.0024620000, S: 0.0032150000, T: -0.0000101400, U: 0.0000173800 },
-    { min: 0.4980, R: -0.0023910000, S: 0.0030740000, T: -0.0000084100, U: 0.0000139800 },
-    { min: 0.5180, R: -0.0022940000, S: 0.0028870000, T: -0.0000083900, U: 0.0000138700 },
-    { min: 0.5390, R: -0.0021460000, S: 0.0026150000, T: -0.0000054600, U: 0.0000085500 },
-    { min: 0.5590, R: -0.0019200000, S: 0.0022140000, T: -0.0000055100, U: 0.0000085900 },
-    { min: 0.5790, R: -0.0023580000, S: 0.0029620000, T: -0.0000122500, U: 0.0002015000 },
-    { min: 0.6000, R: -0.0013610000, S: 0.0013000000, T: -0.0000004900, U: 0.0000006000 },
-    { min: 0.6150, R: -0.0012370000, S: 0.0011000000, T: -0.0000004900, U: 0.0000006000 },
-    { min: 0.6350, R: -0.0010770000, S: 0.0008500000, T: -0.0000004900, U: 0.0000006000 },
-    { min: 0.6550, R: -0.0010110000, S: 0.0007500000, T: -0.0000004900, U: 0.0000006000 },
-    { min: 0.6750, R: -0.0009770000, S: 0.0007000000, T: -0.0000004900, U: 0.0000006000 },
-    { min: 0.6950, R: -0.0010050000, S: 0.0007400000, T: -0.0000004900, U: 0.0000006000 },
-    { min: 0.7460, R: -0.0012380000, S: 0.0010500000, T: -0.0000004900, U: 0.0000006000 },
-    { min: 0.7660, R: -0.0010840000, S: 0.0008500000, T: -0.0000004900, U: 0.0000006000 },
-    { min: 0.7860, R: -0.0009650000, S: 0.0007000000, T: -0.0000004900, U: 0.0000006000 },
-    { min: 0.8060, R: -0.0008435000, S: 0.0005500000, T: -0.0000004900, U: 0.0000006000 },
-    { min: 0.8260, R: -0.0007190000, S: 0.0004000000, T: -0.0000004900, U: 0.0000006000 },
-    { min: 0.8460, R: -0.0006170000, S: 0.0002800000, T: -0.0000004900, U: 0.0000006000 },
-    { min: 0.8710, R: -0.0005120000, S: 0.0001600000, T: -0.0000004900, U: 0.0000006000 },
-    { min: 0.8960, R: -0.0003948000, S: 0.0003000000, T: -0.0000004900, U: 0.0000006000 },
-    { min: 0.9960, R: -0.0005426000, S: 0.0001778000, T: 0.0000023100, U: -0.0000022000 }
-].sort((a, b) => a.min - b.min);
+console.log("Gilmar")
 
-/**
- * Função auxiliar que calcula as constantes V61, X61, W61, Y61 (dependem da Gravidade Observada).
- *
- * @param {number} gravidadeObservada - O valor da Gravidade Observada (O66/Q66).
- * @returns {object} Um objeto contendo as constantes V61, X61, W61, Y61, e Z61.
- */
-function getCorrecaoConstantes(gravidadeObservada, tempAmostra) {
-    // 1. Simulação do PROCV (VLOOKUP) com 'busca aproximada'
-    let linhaCoeficiente = COEFICIENTES_TABELA[0];
-    for (const row of COEFICIENTES_TABELA) {
-        if (gravidadeObservada >= row.min) {
-            linhaCoeficiente = row;
-        } else {
-            break;
-        }
+function tab1a(a){
+let b = a;
+let c = null;
+
+if(b < 0.498){
+    c = -0.002462;
+} else if(b < 0.518){
+    c = -0.0023910;
+} else if(b < 0.539){
+    c = -0.0022940;
+} else if(b < 0.559){
+    c = -0.0021460;
+} else if(b < 0.579){
+    c = -0.0019200;
+} else if(b < 0.600){
+    c = -0.0023580;
+} else if(b < 0.615){
+    c = -0.0013610;
+} else if(b < 0.635){
+    c = -0.0012370;
+} else if(b < 0.655){
+    c = -0.0010770;
+} else if(b < 0.675){
+    c = -0.0010110;
+} else if(b < 0.695){
+    c = -0.0009770;
+} else if(b < 0.746){
+    c = -0.0010050;
+} else if(b < 0.766){
+    c = -0.0012380;
+} else if(b < 0.786){
+    c = -0.0010840;
+} else if(b < 0.806){
+    c = -0.0009650;
+} else if(b < 0.826){
+    c = -0.0008435;
+} else if(b < 0.846){
+    c = -0.0007190;
+} else if(b < 0.871){
+    c = -0.0006170;
+} else if(b < 0.896){
+    c = -0.0005120;
+} else if(b < 0.996){
+    c = -0.0003948;
+} else if(b < 2.000){
+    c = -0.0005426;
+} else {
+   c = 0.0;
+}
+
+return c;
+
+}
+
+/////////////////////////////////////////
+
+function tab2a(a){
+    let b = a;
+    let c = null;
+
+    if(b < 0.498){
+        c = 0.0032150; // TAB2A, 1ª linha
+    } else if(b < 0.518){
+        c = 0.0030740; // TAB2A, 2ª linha
+    } else if(b < 0.539){
+        c = 0.0028870; // TAB2A, 3ª linha
+    } else if(b < 0.559){
+        c = 0.0026150; // TAB2A, 4ª linha
+    } else if(b < 0.579){
+        c = 0.0022140; // TAB2A, 5ª linha
+    } else if(b < 0.600){
+        c = 0.0029620; // TAB2A, 6ª linha
+    } else if(b < 0.615){
+        c = 0.0013000; // TAB2A, 7ª linha
+    } else if(b < 0.635){
+        c = 0.0011000; // TAB2A, 8ª linha
+    } else if(b < 0.655){
+        c = 0.0008500; // TAB2A, 9ª linha
+    } else if(b < 0.675){
+        c = 0.0007500; // TAB2A, 10ª linha
+    } else if(b < 0.695){
+        c = 0.0007000; // TAB2A, 11ª linha
+    } else if(b < 0.746){
+        c = 0.0007400; // TAB2A, 12ª linha
+    } else if(b < 0.766){
+        c = 0.0010500; // TAB2A, 13ª linha
+    } else if(b < 0.786){
+        c = 0.0008500; // TAB2A, 14ª linha
+    } else if(b < 0.806){
+        c = 0.0007000; // TAB2A, 15ª linha
+    } else if(b < 0.826){
+        c = 0.0005500; // TAB2A, 16ª linha
+    } else if(b < 0.846){
+        c = 0.0004000; // TAB2A, 17ª linha
+    } else if(b < 0.871){
+        c = 0.0002800; // TAB2A, 18ª linha
+    } else if(b < 0.896){
+        c = 0.0001600; // TAB2A, 19ª linha
+    } else if(b < 0.996){
+        c = 0.0000300; // TAB2A, 20ª linha
+    } else if(b < 2.000){
+        c = 0.0001778; // TAB2A, 21ª linha (a última na tabela original)
+    } else {
+        // Caso b seja 2.000 ou maior, ou fora do primeiro limite (0.00000 a 0.40000)
+        c = 0.0;
     }
 
-    const R61 = linhaCoeficiente.R; // Coeficiente TAB1A
-    const S61 = linhaCoeficiente.S; // Coeficiente TAB2A
-    const T61 = linhaCoeficiente.T; // Coeficiente TAB1B
-    const U61 = linhaCoeficiente.U; // Coeficiente TAB2B
-    
-    // Variável Delta T (usada apenas para Z61)
-    const deltaT_amostra = tempAmostra - 20;
-
-    // Denominador Comum para V61, X61, W61, Y61
-    const denominadorComum = 1 + 8 * S61 + 64 * U61;
-
-    // V61
-    const numeradorV61 = R61 + 16 * T61 - (8 * R61 + 64 * T61) * U61;
-    const V61 = (9 / 5) * 0.999042 * numeradorV61 / denominadorComum;
-
-    // X61
-    const numeradorX61 = T61 - (8 * R61 + 64 * T61) * U61;
-    const X61 = (81 / 25) * 0.999042 * numeradorX61 / denominadorComum;
-
-    // W61
-    const W61 = (9 / 5) * (S61 + 16 * U61) / denominadorComum;
-
-    // Y61
-    const Y61 = (81 / 25) * U61 / denominadorComum;
-
-    // Z61 (Calculado com base na temperatura da amostra)
-    const Z61 = 1 - 0.000023 * deltaT_amostra - 0.00000002 * Math.pow(deltaT_amostra, 2);
-
-    return { V61, X61, W61, Y61, Z61 };
+    return c;
 }
 
-// -----------------------------------------------------------
-// 1. FUNÇÃO ANTERIOR (Gravity Corrected 20C - Q70)
-// -----------------------------------------------------------
+/////////////////////////////////////////
 
-/**
- * Calcula a Gravidade Corrigida para 20°C (Gravity Corrected 20C - Q70).
- *
- * @param {number} gravidadeObservada - O valor da Gravidade Observada (O66/Q66).
- * @param {number} tempAmostra - A Temperatura da Amostra (O67).
- * @returns {number} O valor da Gravidade Corrigida a 20C (Q70).
- */
-function calcularGravidadeCorrigida(gravidadeObservada, tempAmostra) {
-    const { V61, X61, W61, Y61, Z61 } = getCorrecaoConstantes(gravidadeObservada, tempAmostra);
-    const deltaT = tempAmostra - 20;
+function tab1b(a){
+    let b = a;
+    let c = null;
 
-    // Q70 = (O66 - V61*deltaT - X61*deltaT^2) / (1 + W61*deltaT + Y61*deltaT^2) * Z61
-    const numeradorQ70 = gravidadeObservada - V61 * deltaT - X61 * Math.pow(deltaT, 2);
-    const denominadorQ70 = 1 + W61 * deltaT + Y61 * Math.pow(deltaT, 2);
-    
-    return (numeradorQ70 / denominadorQ70) * Z61;
+    if(b < 0.498){
+        c = -0.000010140; // TAB1B, 1ª linha
+    } else if(b < 0.518){
+        c = -0.000008410; // TAB1B, 2ª linha
+    } else if(b < 0.539){
+        c = -0.000008390; // TAB1B, 3ª linha
+    } else if(b < 0.559){
+        c = -0.000005460; // TAB1B, 4ª linha
+    } else if(b < 0.579){
+        c = -0.000005510; // TAB1B, 5ª linha
+    } else if(b < 0.600){
+        c = -0.0000012250; // TAB1B, 6ª linha
+    } else if(b < 0.615){
+        c = -0.0000000490; // TAB1B, 7ª linha
+    } else if(b < 0.635){
+        c = -0.0000000490; // TAB1B, 8ª linha
+    } else if(b < 0.655){
+        c = -0.0000000490; // TAB1B, 9ª linha
+    } else if(b < 0.675){
+        c = -0.0000000490; // TAB1B, 10ª linha
+    } else if(b < 0.695){
+        c = -0.0000000490; // TAB1B, 11ª linha
+    } else if(b < 0.746){
+        c = -0.0000000490; // TAB1B, 12ª linha
+    } else if(b < 0.766){
+        c = -0.0000000490; // TAB1B, 13ª linha
+    } else if(b < 0.786){
+        c = -0.0000000490; // TAB1B, 14ª linha
+    } else if(b < 0.806){
+        c = -0.0000000490; // TAB1B, 15ª linha
+    } else if(b < 0.826){
+        c = -0.0000000490; // TAB1B, 16ª linha
+    } else if(b < 0.846){
+        c = -0.0000000490; // TAB1B, 17ª linha
+    } else if(b < 0.871){
+        c = -0.0000000490; // TAB1B, 18ª linha
+    } else if(b < 0.896){
+        c = -0.0000000490; // TAB1B, 19ª linha
+    } else if(b < 0.996){
+        c = -0.0000000490; // TAB1B, 20ª linha
+    } else if(b < 2.000){
+        c = 0.0000002310; // TAB1B, 21ª linha (a última na tabela original)
+    } else {
+        // Caso b seja 2.000 ou maior, ou fora do primeiro limite.
+        c = 0.0;
+    }
+
+    return c;
 }
 
-// -----------------------------------------------------------
-// 2. NOVA FUNÇÃO (Volume Conversion Factor - Q71)
-// -----------------------------------------------------------
+/////////////////////////////////////////
+function tab2b(a){
+    let b = a;
+    let c = null;
 
-/**
- * Calcula o Fator de Conversão de Volume (Volume Conversion Factor - Q71).
- *
- * @param {number} gravidadeObservada - O valor da Gravidade Observada (O66/Q66).
- * @param {number} tempAmostra - A Temperatura da Amostra (O67).
- * @param {number} tempAmbiente - A Temperatura Ambiente (Q68).
- * @returns {number} O Fator de Conversão de Volume (Q71).
- */
-function calcularFatorConversaoVolume(gravidadeObservada, tempAmostra, tempAmbiente) {
-    // 1. Obter o resultado Q70
-    const Q70 = calcularGravidadeCorrigida(gravidadeObservada, tempAmostra);
-    
-    // 2. Obter as constantes que dependem apenas da Gravidade (V61, X61, W61, Y61)
-    const { V61, X61, W61, Y61 } = getCorrecaoConstantes(gravidadeObservada, tempAmostra);
-    
-    const deltaT_ambiente = tempAmbiente - 20;
+    if(b < 0.498){
+        c = 0.00001738; // TAB2B, 1ª linha
+    } else if(b < 0.518){
+        c = 0.00001398; // TAB2B, 2ª linha
+    } else if(b < 0.539){
+        c = 0.00001387; // TAB2B, 3ª linha
+    } else if(b < 0.559){
+        c = 0.00000855; // TAB2B, 4ª linha
+    } else if(b < 0.579){
+        c = 0.00000855; // TAB2B, 5ª linha
+    } else if(b < 0.600){
+        c = 0.00020150; // TAB2B, 6ª linha
+    } else if(b < 0.615){
+        c = 0.00000060; // TAB2B, 7ª linha
+    } else if(b < 0.635){
+        c = 0.00000060; // TAB2B, 8ª linha
+    } else if(b < 0.655){
+        c = 0.00000060; // TAB2B, 9ª linha
+    } else if(b < 0.675){
+        c = 0.00000060; // TAB2B, 10ª linha
+    } else if(b < 0.695){
+        c = 0.00000060; // TAB2B, 11ª linha
+    } else if(b < 0.746){
+        c = 0.00000060; // TAB2B, 12ª linha
+    } else if(b < 0.766){
+        c = 0.00000060; // TAB2B, 13ª linha
+    } else if(b < 0.786){
+        c = 0.00000060; // TAB2B, 14ª linha
+    } else if(b < 0.806){
+        c = 0.00000060; // TAB2B, 15ª linha
+    } else if(b < 0.826){
+        c = 0.00000060; // TAB2B, 16ª linha
+    } else if(b < 0.846){
+        c = 0.00000060; // TAB2B, 17ª linha
+    } else if(b < 0.871){
+        c = 0.00000060; // TAB2B, 18ª linha
+    } else if(b < 0.896){
+        c = 0.00000060; // TAB2B, 19ª linha
+    } else if(b < 0.996){
+        c = 0.00000060; // TAB2B, 20ª linha
+    } else if(b < 2.000){
+        c = -0.00000220; // TAB2B, 21ª linha (a última na tabela original)
+    } else {
+        // Caso b seja 2.000 ou maior, ou fora do primeiro limite.
+        c = 0.0;
+    }
 
-    // Numerador Q71 (parte do termo 2): (V61*deltaT_ambiente + X61*deltaT_ambiente^2)
-    const numerador_termo2 = V61 * deltaT_ambiente + X61 * Math.pow(deltaT_ambiente, 2);
-
-    // Termo 1: (1 + W61*deltaT_ambiente + Y61*deltaT_ambiente^2)
-    const termo1 = 1 + W61 * deltaT_ambiente + Y61 * Math.pow(deltaT_ambiente, 2);
-    
-    // Termo 2: (Numerador_termo2 / Q70)
-    const termo2 = numerador_termo2 / Q70;
-
-    // Q71 = Termo 1 + Termo 2
-    return termo1 + termo2;
+    return c;
 }
-
-// --- EXEMPLO DE USO COM OS SEUS DADOS DE ENTRADA ---
-const GRAVIDADE = 0.8760;        // Q66
-const TEMPERATURA_AMOSTRA = 24.300; // Q67
-const TEMPERATURA_AMBIENTE = 49.0;  // Q68
-
-// A. Calculando a Gravidade Corrigida (Q70)
-const Q70_RESULTADO = calcularGravidadeCorrigida(GRAVIDADE, TEMPERATURA_AMOSTRA);
-
-// B. Calculando o Fator de Conversão de Volume (Q71)
-const Q71_RESULTADO = calcularFatorConversaoVolume(GRAVIDADE, TEMPERATURA_AMOSTRA, TEMPERATURA_AMBIENTE);
-
-console.log(`Gravidade Corrigida 20C (Q70): ${Q70_RESULTADO}`); 
-console.log(`Fator de Conversão de Volume (Q71): ${Q71_RESULTADO}`);
